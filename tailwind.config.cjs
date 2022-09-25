750960a7c74c8fc97bc3cad9aff186925660d08c
Aaron Lee <longyklee@gmail.com>
2022-09-25 15:54:53 +0800

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'c1': '#041B15',
+				'c2': '#136F63',
+				'c3': '#22AAA1',
+				'c4': '#4CE0D2',
+				'c5': '#84CAE7',
+				'dc2': '#05332D',
+				'lc4': '#DCFFFC',
+				'llc4': '#f5fffe',
+				'bg': '#EEF6DD',
+			},
+		},
+		container: {
+				padding: '1rem',
+			},
+		},
+	plugins: [],
+}
