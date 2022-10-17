c933f4a019123146541d37bd2ece1240cc895e48
Aaron Lee <longyklee@gmail.com>
2022-10-17 21:48:39 +0800

    update astro & fixed image problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
