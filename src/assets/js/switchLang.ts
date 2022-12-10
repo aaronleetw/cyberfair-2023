e10f7b49f2975b36f50ea3de0e0abb6ea5d36057
Aaron Lee <longyklee@gmail.com>
2022-12-10 23:53:16 +0800

    add chinese

---
+++ src/assets/js/switchLang.ts
@@ -0,0 +1,8 @@
+export default switchLang;
+
+function switchLang(currurl: string, lang: string) {
+	let currPath = currurl == '/' ? '/' : currurl.substring(3);
+	if (lang == 'en' && currPath == '/') { return '/'; }
+	// if (lang == 'zh' && currPath == '/narrative.html') { location.href = '/zh/'; return; }
+	return "/" + lang + currPath;
+}
