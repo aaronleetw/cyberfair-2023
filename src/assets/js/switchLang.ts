22b59d49ef8d4ffc88f1ee2235cef1519e383001
Aaron Lee <longyklee@gmail.com>
2022-12-18 20:17:09 +0800

    final update

--- src/assets/js/switchLang.ts
+++ src/assets/js/switchLang.ts
@@ -2,7 +2,7 @@ export default switchLang;
 
 function switchLang(currurl: string, lang: string) {
 	let currPath = currurl == '/' ? '/' : currurl.substring(3);
-	if (lang == 'en' && currPath == '/') { return '/'; }
+	if (lang == 'en' && currPath == '') { return '/'; }
 	// if (lang == 'zh' && currPath == '/narrative.html') { location.href = '/zh/'; return; }
 	return "/" + lang + currPath;
 }
