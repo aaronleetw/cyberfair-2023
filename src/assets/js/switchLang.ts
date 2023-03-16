03d89a907ae0d3241b01d5a5a975d59d9aca1c45
Aaron Lee <longyklee@gmail.com>
2023-03-17 01:11:04 +0800

    finalizing changes

--- src/assets/js/switchLang.ts
+++ src/assets/js/switchLang.ts
@@ -3,6 +3,6 @@ export default switchLang;
 function switchLang(currurl: string, lang: string) {
 	let currPath = currurl == '/' ? '/' : currurl.substring(3);
 	if (lang == 'en' && currPath == '') { return '/'; }
-	// if (lang == 'zh' && currPath == '/narrative.html') { location.href = '/zh/'; return; }
+	if (currurl == '/narrative') { return '/narrative'; }
 	return "/" + lang + currPath;
 }
